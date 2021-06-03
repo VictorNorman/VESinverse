42fd07b8dfae3b5d995e296a792b0e4285ae101a
ajv234 <ajv234@maroon20.cs.calvin.edu>
2021-06-03 12:27:01 -0400

    Cleaned up with pycodestyle

--- VESinverse_ajv234/VESinverse_ajv234.py
+++ VESinverse_ajv234/VESinverse_ajv234.py
@@ -17,7 +17,7 @@ import sys
 
 
 class VESinverse:
-    def __init__(self): 
+    def __init__(self):
         # For Testing Purposes
         # Only the Test suit should edit these Constants
         self.LAYERS = 4
@@ -29,27 +29,30 @@ class VESinverse:
 
         # Schlumberger filter
         self.fltr1 = [.00046256, -.0010907, .0017122, -.0020687,
-                .0043048, -.0021236, .015995, .017065, .098105, .21918, .64722,
-                1.1415, .47819, -3.515, 2.7743, -1.201, .4544, -.19427, .097364,
-                -.054099, .031729, -.019109, .011656, -.0071544, .0044042,
-                -.002715, .0016749, -.0010335, .00040124]
+                      .0043048, -.0021236, .015995, .017065, .098105,
+                      .21918, .64722, 1.1415, .47819, -3.515, 2.7743,
+                      -1.201, .4544, -.19427, .097364, -.054099, .031729,
+                      -.019109, .011656, -.0071544, .0044042,
+                      -.002715, .0016749, -.0010335, .00040124]
 
         # Wenner Filter
         self.fltr2 = [.000238935, .00011557, .00017034, .00024935,
-                .00036665, .00053753, .0007896, .0011584, .0017008, .0024959,
-                .003664, .0053773, .007893, .011583, .016998, .024934, .036558,
-                .053507, .078121, .11319, .16192, .22363, .28821, .30276, .15523,
-                -.32026, -.53557, .51787, -.196, .054394, -.015747, .0053941,
-                -.0021446, .000665125]
+                      .00036665, .00053753, .0007896, .0011584, .0017008,
+                      .0024959, .003664, .0053773, .007893, .011583,
+                      .016998, .024934, .036558, .053507, .078121, .11319,
+                      .16192, .22363, .28821, .30276, .15523,
+                      -.32026, -.53557, .51787, -.196, .054394, -.015747,
+                      .0053941, -.0021446, .000665125]
 
-        # I know there must be a better method to assign lists. And probably numpy
-        # arrays would be best. But my Python wasn't up to it. If the last letter
+        # I know there must be a better method to assign lists.
+        # And probably numpy arrays would be best.
+        # But my Python wasn't up to it. If the last letter
         # is an 'l' that means it is a log10 of the value
 
         # 65 is completely arbitrary
-        self.p = [0]*20                              #Prediction?
-        self.r = [0]*ARRAYSIZE                       #Resistivity?
-        self.rl = [0]*ARRAYSIZE                      #Resistivity? log10
+        self.p = [0]*20                              # Prediction?
+        self.r = [0]*ARRAYSIZE                       # Resistivity?
+        self.rl = [0]*ARRAYSIZE                      # Resistivity?
         self.t = [0]*50
         self.b = [0]*ARRAYSIZE
         self.asav = [0]*ARRAYSIZE
@@ -78,34 +81,35 @@ class VESinverse:
         self.ndat = 12
 
     def data_init(self):
-        # hard coded data input - spacing and apparent resistivities measured
-        # in teh field
-        self.adat = [0.55, 0.95, 1.5, 2.5, 3., 4.5, 5.5, 9., 12., 20., 30., 70.]
+        # hard coded data input - spacing and
+        # apparent resistivities measured in the field
+        self.adat = [0.55, 0.95, 1.5, 2.5, 3., 4.5,
+                     5.5, 9., 12., 20., 30., 70.]
 
         if self.DATASET == 1:
             self.rdat = [125., 110., 95., 40., 24., 15.,
-                    10.5, 8., 6., 6.5, 11., 25.]  # DATA 1
+                         10.5, 8., 6., 6.5, 11., 25.]  # DATA 1
         elif self.DATASET == 2:
             self.rdat = [125., 130., 140., 150., 160.,
-                    170., 175., 170., 130., 100., 80., 60.]  # DATA 2
+                         170., 175., 170., 130., 100., 80., 60.]  # DATA 2
         elif self.DATASET == 3:
             self.rdat = [125., 124., 120., 115., 110.,
-                    95., 40., 24., 15., 10., 11., 25.]  # DATA 3
+                         95., 40., 24., 15., 10., 11., 25.]  # DATA 3
         elif self.DATASET == 4:
-            self.rdat = [ 125., 124., 126., 129., 135.,
-                    140., 150., 170., 175., 180., 185., 187.]  # DATA 4
+            self.rdat = [125., 124., 126., 129., 135.,
+                         140., 150., 170., 175., 180., 185., 187.]  # DATA 4
         elif self.DATASET == 5:
-            self.rdat = [ 125., 124., 126., 122., 120.,
-                    110., 85., 65., 40., 30., 26., 25.]  # DATA 5
+            self.rdat = [125., 124., 126., 122., 120.,
+                         110., 85., 65., 40., 30., 26., 25.]  # DATA 5
         elif self.DATASET == 6:
-            self.rdat = [ 125., 124., 126., 129., 135.,
-                    180., 220., 250., 280., 300., 310., 315.]  # DATA 6
+            self.rdat = [125., 124., 126., 129., 135.,
+                         180., 220., 250., 280., 300., 310., 315.]  # DATA 6
         elif self.DATASET == 7:
             self.rdat = [300., 303., 330., 330., 310.,
-                    300., 285., 240., 205., 180., 180., 210.]  # DATA 7
+                         300., 285., 240., 205., 180., 180., 210.]  # DATA 7
         elif self.DATASET == 8:
             self.rdat = [300., 298., 290., 270., 280.,
-                    300., 330., 370., 420., 510., 507., 370.]  # DATA 8
+                         300., 330., 370., 420., 510., 507., 370.]  # DATA 8
         self.one30 = 1.e30
         self.rms = self.one30
         self.errmin = 1.e10
@@ -120,12 +124,13 @@ class VESinverse:
         elif self.LAYERS == 4:
             self.layer = 4
 
-        self.layer_index = 2 * self.layer - 1     # layer (e) and layer_index (n) variables have been updated
+        # layer (e) and layer_index (n) variables have been updated
+        self.layer_index = 2 * self.layer - 1
 
-        # self.layer_index -= 1   #since this is an index, it should be reduced by 1 to fit with indexing by 0
-
-        self.electrode_spacing = 0.2  # smallest electrode spacing
-        self.resistivity_points_number = 20  # number of points where resistivity is calculated (Variable was m)
+        # smallest electrode spacing
+        self.electrode_spacing = 0.2
+        # number of points where resistivity is calculated (Variable was m)
+        self.resistivity_points_number = 20
 
         self.electrode_spacing = np.log(self.electrode_spacing)
         self.delx = np.log(10.0)/6.
@@ -233,8 +238,8 @@ class VESinverse:
             self.small[6] = 1.
             self.xlarge[6] = 500.
 
-        self.iter = 10000  # number of iterations for the Monte Carlo guesses. to be input on GUI
-
+        # number of iterations for the Monte Carlo guesses. to be input on GUI
+        self.iter = 10000
 
     def readData(self):
         # normally this is where the data would be read from the csv file
@@ -246,13 +251,14 @@ class VESinverse:
 
         return
 
-
     def error(self):
         sumerror = 0.
         # pltanswer = [0]*64
-        self.spline(self.resistivity_points_number, self.one30, self.one30, self.asavl, self.rl, self.y2)
+        self.spline(self.resistivity_points_number, self.one30, self.one30,
+                    self.asavl, self.rl, self.y2)
         for i in range(0, self.ndat, 1):
-            ans = self.splint(self.resistivity_points_number, self.adatl[i], self.asavl, self.rl, self.y2)
+            ans = self.splint(self.resistivity_points_number, self.adatl[i],
+                              self.asavl, self.rl, self.y2)
             sumerror = sumerror + (self.rdatl[i] - ans) * (self.rdatl[i] - ans)
             # print(i,sum1,rdat[i],rdatl[i],ans)
             self.pltanswerl[i] = ans
@@ -269,7 +275,6 @@ class VESinverse:
         # plt.loglog(adat[1:ndat],pltanswer[1:ndat])
         return self.rms
 
-
     def transf(self, y, i):
         self.u = 1./np.exp(y)
         self.t[0] = self.p[self.layer_index-1]
@@ -283,10 +288,10 @@ class VESinverse:
             b = (1. - a)/(1. + a)
             rs = self.p[self.layer_index - 1 - j]
             tpr = b*rs
-            self.t[j] = (tpr + self.t[j - 1]) / (1. + tpr * self.t[j - 1] / (rs * rs))
+            self.t[j] = (tpr + self.t[j - 1]) / (1. + tpr * self.t[j - 1] /
+                                                 (rs * rs))
         self.r[i] = self.t[self.layer-1]
         return
-
 
     def filters(self, b, k):
         for i in range(0, self.resistivity_points_number, 1):
@@ -296,10 +301,9 @@ class VESinverse:
             self.r[i] = re
         return
 
-
     def rmsfit(self):
         if self.index == 1:
-            self.y = self.electrode_spacing -19. * self.delx - 0.13069
+            self.y = self.electrode_spacing - 19. * self.delx - 0.13069
             mum1 = self.resistivity_points_number + 28
             for i in range(0, mum1, 1):
                 self.transf(self.y, i)
@@ -307,7 +311,7 @@ class VESinverse:
             self.filters(self.fltr1, 29)
         elif self.index == 2:
             s = np.log(2.)
-            self.y = self.electrode_spacing -10.8792495 * self.delx
+            self.y = self.electrode_spacing - 10.8792495 * self.delx
             mum2 = self.resistivity_points_number + 33
             for i in range(0, mum2, 1):
                 self.transf(self.y, i)
@@ -341,9 +345,10 @@ class VESinverse:
     # to prediction
 
     # What does this do? It doesn't modify any self. variable
-    # x, y, y2 and p are the only ones that are self. variables and the first three are local
-    # and the operations done on p mean that it cannot be self.p because it is an array
-    def spline(self, n, yp1, ypn, x=[], y=[], y2=[]):       # n in this case is one30
+    # x, y, y2 and p are the only ones that are self. variables and the first
+    # three are local and the operations done on p mean that it cannot be
+    # self.p because it is an array
+    def spline(self, n, yp1, ypn, x=[], y=[], y2=[]):
         u = [0] * 1000
         one29 = 0.99e30
         # print(x,y)
@@ -359,22 +364,24 @@ class VESinverse:
             sig = (x[i] - x[i - 1]) / (x[i + 1] - x[i - 1])
             p = sig * y2[i - 1] + 2.
             y2[i] = (sig - 1.) / p
-            u[i] = ((6. * ((y[i + 1] - y[i]) / (x[i + 1] - x[i]) - (y[i] - y[i - 1]) /
-                        (x[i] - x[i - 1])) / (x[i + 1] - x[i - 1]) - sig * u[i - 1]) / p)
+            u[i] = ((6. * ((y[i + 1] - y[i]) / (x[i + 1] - x[i]) -
+                           (y[i] - y[i - 1]) /
+                           (x[i] - x[i - 1])) / (x[i + 1] - x[i - 1]) - sig *
+                     u[i - 1]) / p)
 
         if ypn > one29:
             qn = 0.
             un = 0.
         else:
             qn = 0.5
-            un = (3. / (x[n] - x[n - 1])) * (ypn - (y[n] - y[n - 1]) / (x[n] - x[n - 1]))
+            un = (3. / (x[n] - x[n - 1])) * (ypn - (y[n] - y[n - 1]) /
+                                             (x[n] - x[n - 1]))
 
         y2[n] = (un - qn * u[n - 1]) / (qn * y2[n - 1] + 1.)
         for k in range(n-1, -1, -1):
             y2[k] = y2[k] * y2[k + 1] + u[k]
 
         return
-
 
     def splint(self, n, x, xa=[], ya=[], y2a=[]):
         klo = 0
@@ -392,11 +399,12 @@ class VESinverse:
         a = (xa[khi] - x) / h
         b = (x - xa[klo]) / h
         y = (a * ya[klo] + b * ya[khi] + ((a * a * a - a) * y2a[klo] +
-                                (b * b * b - b) * y2a[khi]) * (h * h) / 6.)
-        # print("x=   ", x,"y=  ", y, "  ya=  ", ya[khi],"  y2a=  ", y2a[khi], "  h=  ",h)
+                                          (b * b * b - b) * y2a[khi]) *
+             (h * h) / 6.)
+        # print("x=   ", x,"y=  ", y, "  ya=  ", ya[khi],"
+        #       y2a=  ", y2a[khi], "  h=  ",h)
 
         return y
-
 
     def computePredictions(self):
 
@@ -429,7 +437,7 @@ class VESinverse:
     # output the best fitting earth model
         print(' Layer ', '     Thickness  ', '   Res_ohm-m  ')
         for i in range(0, self.layer - 1, 1):
-            print(i, self.pkeep[i], self.pkeep[self.layer+i-1])     #self.pkeep[self.layer+i-1] needs to be changed
+            print(i, self.pkeep[i], self.pkeep[self.layer+i-1])
 
         print(self.layer, '  Infinite ', self.pkeep[self.layer_index-1])
         for i in range(0, self.resistivity_points_number, 1):
@@ -441,14 +449,14 @@ class VESinverse:
         for i in range(0, self.resistivity_points_number, 1):
             # print(asav[i], rkeep[i], asavl[i], rkeepl[i])
             print("%9.3f   %9.3f  %9.3f  %9.3f" % (self.asav[i], self.rkeep[i],
-                                                self.asavl[i], self.rkeepl[i]))
+                                                   self.asavl[i], self.rkeepl[i]))
 
         plt.loglog(self.asav[1:self.resistivity_points_number], self.rkeep[1:self.resistivity_points_number], '-')  # resistivity prediction curve
         plt.loglog(self.adat[1:self.ndat], self.pltanswerkeep[1:self.ndat],
-                'ro')  # predicted data red dots
+                   'ro')  # predicted data red dots
         s = 7
         plt.loglog(self.adat[1:self.ndat], self.rdat[1:self.ndat], 'bo',
-                markersize=s)  # original data blue dots
+                   markersize=s)  # original data blue dots
 
         # output the ranges cpmstraining the model
 

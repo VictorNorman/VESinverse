937b6af2b1fc6e7652ac39c0cea8d22762918d5c
zer0relm <zer0relm@gmail.com>
2021-06-11 15:25:20 -0400

    spliting file

--- brython/main.py
+++ brython/main.py
@@ -1,10 +1,23 @@
 from browser import bind, window, document, html
+from VESinverse import VESinverse
 
 def read_file(e):
         def onload(e):
             document['file-text'].value = e.target.result
-            print(document['file-text'].value[22])
-        
+            my_str = e.target.result
+            file_list = my_str.split("\n")
+            print(file_list)
+
+            # line 1 chooses what algorithm to run
+            if int(file_list[1]) == 1:
+                algorithm_choice = 1
+            elif int(file_list[1]) == 2:
+                algorithm_choice = 2
+            VI.set_index(algorithm_choice)
+
+            data_length = len(file_list) - 3
+            VI.set_ndat(data_length)
+
         resistivity_file = document['file-upload'].files[0]
         reader = window.FileReader.new()
         reader.readAsText(resistivity_file)
@@ -13,4 +26,5 @@ def read_file(e):
         # print(resistivity_file.path)
         reader.bind('load', onload)
 
+VI = VESinverse()
 document['file-upload'].bind('input', read_file)

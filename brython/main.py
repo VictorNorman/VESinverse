15e427f7f0cafb92ea6933841aa9bf2d58ad3f45
zer0relm <zer0relm@gmail.com>
2021-06-14 11:43:17 -0400

    Added layer dropdown and thick and res inputs

--- brython/main.py
+++ brython/main.py
@@ -1,7 +1,14 @@
 from browser import bind, window, document, html
-from VESinverse import VESinverse
+# from VESinverse import VESinverse
 
-def read_file(e):
+class web_gui:
+    def __init__(self):
+        # VI = VESinverse()
+        document['file-upload'].bind('input', self.read_file)
+        self.layer_dropdown()
+        self.inputs()
+
+    def read_file(self, e):
         def onload(e):
             document['file-text'].value = e.target.result
             my_str = e.target.result
@@ -9,14 +16,14 @@ def read_file(e):
             print(file_list)
 
             # line 1 chooses what algorithm to run
-            if int(file_list[1]) == 1:
-                algorithm_choice = 1
-            elif int(file_list[1]) == 2:
-                algorithm_choice = 2
-            VI.set_index(algorithm_choice)
+            # if int(file_list[1]) == 1:
+            #     algorithm_choice = 1
+            # elif int(file_list[1]) == 2:
+            #     algorithm_choice = 2
+            # VI.set_index(algorithm_choice)
 
-            data_length = len(file_list) - 3
-            VI.set_ndat(data_length)
+            # data_length = len(file_list) - 3
+            # VI.set_ndat(data_length)
 
         resistivity_file = document['file-upload'].files[0]
         reader = window.FileReader.new()
@@ -26,5 +33,26 @@ def read_file(e):
         # print(resistivity_file.path)
         reader.bind('load', onload)
 
-VI = VESinverse()
-document['file-upload'].bind('input', read_file)
+    def layer_dropdown(self):
+        document['layer_choice'] <= "Choose layer: "
+        dropdown = html.SELECT(html.OPTION(i) for i in range(6))
+        dropdown.bind("change", self.changeLayer)
+        document['layer_choice'] <= dropdown
+
+    def changeLayer(self, e):
+        print("Layer will be changed")
+
+    def inputs(self):
+        for i in range(2):
+            thickness_inputs = html.INPUT(type = "Text", id = f"thick_min{i}")
+            document['thick_input'] <= thickness_inputs
+            thickness_inputs = html.INPUT(type = "Text", id = f"thick_max{i}")
+            document['thick_input'] <= thickness_inputs
+            document['thick_input'] <= html.BR()
+        for i in range(3):
+            resistivity_inputs = html.INPUT(type = "Text", id = f"res_min{i}")
+            document['res_input'] <= resistivity_inputs
+            resistivity_inputs = html.INPUT(type = "Text", id = f"res_max{i}")
+            document['res_input'] <= resistivity_inputs
+            document['res_input'] <= html.BR()
+        
